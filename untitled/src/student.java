6c994af9bb7c8c1be833257da1563f0fcc6d070c
qijie123 <1823847162@qq.com>
2022-07-27 14:12:12 +0800

    'd'

---
+++ untitled/src/student.java
@@ -0,0 +1,15 @@
+public class student {
+
+    /**
+     * 上网课
+     * 上晚自习
+     * 我在图书馆读书
+     */
+
+
+
+    public void goToNightClass() {System.out.println("我们在认真的上晚自习");}
+    public void immerseOneselfInTheLibrary() {
+        System.out.println("我在图书馆读书很开心");
+    }
+}
